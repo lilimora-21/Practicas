3f48ed2ebfbacd1f929580dbd5f61bd94d8159cf
lilimora-21 <A24310206@live.ceti.mx>
2025-04-12 01:34:15 +0000

    Practica 5

--- binario.cpp
+++ binario.cpp
@@ -3,26 +3,37 @@
 #include <string>
 using namespace std;
 
+struct Persona 
+{
+    char nombre[50];
+    int edad;
+};
+
+
 int main(int argc, char const *argv[])
 {
-
-   
         ofstream archivoSalida;
 
-
-
         //Abrir el archivo de salida 
-    archivoSalida.open("archivo_salida.txt");
+    archivoSalida.open("archivo_salida.bin",ios::binary);
     if (!archivoSalida) {
         cerr << "Error al abrir el archivo de salida." <<endl;
         return 1;
 
     }
+    Persona p1 = {"Juan", 30};
+    Persona p2 = {"Maria", 25};
+
+
     //Escribir en el achivo de salida
-    archivoSalida << "Hola, este es un archivo de salida." << endl;
+    archivoSalida.write((const char*)(&p1), sizeof(Persona));
+    archivoSalida.write((const char*)(&p2), sizeof(Persona));
+    
 
     //Cerrar el archivo de salida 
     archivoSalida.close();
+
+    return 0;
 
     //Leer archivos de salida
 
@@ -31,7 +42,7 @@ int main(int argc, char const *argv[])
 
     //abrir el archivo de entrada
     
-    archivoEntrada.open("archivo_salida.txt");
+    archivoEntrada.open("archivo_salida.bin");
     if (!archivoEntrada) {
         cerr << "Error al abrir el archivo de entrada." <<endl;
         return 1;
--- makefile
+++ makefile
@@ -16,3 +16,8 @@ bin/archivos: src/archivos.cpp
 archivos : bin/archivos 
 	./bin/archivos
 	
+bin/binario : src/binario.cpp
+	g++ src/binario.cpp -o bin/binario
+
+binario : bin/binario 
+		./bin/binario 
